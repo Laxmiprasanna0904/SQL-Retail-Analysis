bc74ef035970d18d8e7e71c30026168582224a6f
Laxmiprasanna0904 <145340110+Laxmiprasanna0904@users.noreply.github.com>
2026-04-22 17:27:12 +0530

    Update sql_queries.sql

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,1 +1,29 @@
+-- Total Revenue
+SELECT SUM(Sales) AS total_revenue FROM orders;
 
+-- Total Profit
+SELECT SUM(Profit) AS total_profit FROM orders;
+
+-- Sales by Category
+SELECT Category, SUM(Sales) AS revenue
+FROM orders
+GROUP BY Category;
+
+-- Top 5 Products by Sales
+SELECT Product_Name, SUM(Sales) AS revenue
+FROM orders
+GROUP BY Product_Name
+ORDER BY revenue DESC
+LIMIT 5;
+
+-- Sales by Region
+SELECT Region, SUM(Sales) AS revenue
+FROM orders
+GROUP BY Region;
+
+-- Monthly Sales Trend
+SELECT DATE_TRUNC('month', Order_Date) AS month,
+SUM(Sales) AS revenue
+FROM orders
+GROUP BY month
+ORDER BY month;
